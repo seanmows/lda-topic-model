c612a7b5fa2fd7e3c5f94d378c13300ad5693a7a
Sean Mowrey <smowrey@ea.com>
2019-07-08 15:41:09 -0700

    first commit

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const Lda = require('./lib/index');
-let rawdata = fs.readFileSync('./testInput.json');  
-let document = JSON.parse(rawdata);  
-const lda = new Lda(null, document, 50, 10);
+let rawdata = fs.readFileSync('./testInput.json');
+let document = JSON.parse(rawdata);
+const lda = new Lda(null, document);
 console.log(lda.displayTopicWords());
+console.log('***Test Complete***');
 
